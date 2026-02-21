71f44c6f97148a5bc87a1ea8fcfa4f36dd5bebd3
Thomas Aglassinger <roskakori@users.sourceforge.net>
2026-02-21 12:56:25 +0100

    #23 Move tests from unittest to pytest

--- ebcdic/ebcdic/test/test_ebcdic.py
+++ ebcdic/ebcdic/test/test_ebcdic.py
@@ -3,61 +3,57 @@ Basic test for `ebcdic` package.
 """
 
 import codecs
-import unittest
+
+import pytest
 
 import ebcdic
 
 
-class EbcdicTest(unittest.TestCase):
-    """
-    Test case of `ebcdic` package.
-    """
-
-    def test_has_codecs(self):
-        self.assertTrue(len(ebcdic.codec_names) > 0)
-        self.assertTrue("cp1141" in ebcdic.codec_names)
-
-    def test_can_recode_hello_world(self):
-        self._test_can_recode("hello world", ebcdic.codec_names)
-
-    def _test_can_recode(self, text, codec_names):
-        for codec_name in codec_names:
-            encoded_text = text.encode(codec_name)
-            recoded_text = encoded_text.decode(codec_name)
-            self.assertEqual(text, recoded_text, f"{codec_name}: {text!r} != {recoded_text!r}")
-
-    def test_can_recode_euro_sign(self):
-        self._test_can_recode("\N{EURO SIGN}", ["cp1141", "cp1148", "cp1148ms"])
-
-    def test_has_ignored_codec_names(self):
-        self.assertTrue("cp500" in ebcdic.ignored_codec_names())
-
-    def test_can_lookup_ebcdic_codec(self):
-        ebcdic_cp500 = ebcdic.lookup("cp500")
-        standard_cp500 = codecs.lookup("cp500")
-        self.assertNotEqual(ebcdic_cp500, standard_cp500)
-
-    def test_fails_on_lookup_of_unknown_ebcdic_codec(self):
-        self.assertRaises(LookupError, ebcdic.lookup, "cpNone")
-
-    def test_ignored_codecs_are_identical_to_standard_library(self):
-        def encoded(code, codec):
-            return chr(code).encode(codec.name, errors="replace")
-
-        for codec_name in ebcdic.ignored_codec_names():
-            ebcdic_codec = ebcdic.lookup(codec_name)
-            self.assertNotEqual(ebcdic_codec, None, codec_name)
-            standard_codec = codecs.lookup(codec_name)
-            self.assertNotEqual(standard_codec, None, codec_name)
-            for code in range(256):
-                ebcdic_char = encoded(code, ebcdic_codec)
-                standard_char = encoded(code, standard_codec)
-                self.assertEqual(
-                    ebcdic_char,
-                    standard_char,
-                    f"{codec_name} at {code}: {ebcdic_char!r} != {standard_char!r}",
-                )
+def test_has_codecs():
+    assert len(ebcdic.codec_names) > 0
+    assert "cp1141" in ebcdic.codec_names
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_can_recode_hello_world():
+    _test_can_recode("hello world", ebcdic.codec_names)
+
+
+def _test_can_recode(text, codec_names):
+    for codec_name in codec_names:
+        encoded_text = text.encode(codec_name)
+        recoded_text = encoded_text.decode(codec_name)
+        assert text == recoded_text, f"{codec_name}: {text!r} != {recoded_text!r}"
+
+
+def test_can_recode_euro_sign():
+    _test_can_recode("\N{EURO SIGN}", ["cp1141", "cp1148", "cp1148ms"])
+
+
+def test_has_ignored_codec_names():
+    assert "cp500" in ebcdic.ignored_codec_names()
+
+
+def test_can_lookup_ebcdic_codec():
+    ebcdic_cp500 = ebcdic.lookup("cp500")
+    standard_cp500 = codecs.lookup("cp500")
+    assert ebcdic_cp500 != standard_cp500
+
+
+def test_fails_on_lookup_of_unknown_ebcdic_codec():
+    with pytest.raises(LookupError):
+        ebcdic.lookup("cpNone")
+
+
+def test_ignored_codecs_are_identical_to_standard_library():
+    def encoded(code, codec):
+        return chr(code).encode(codec.name, errors="replace")
+
+    for codec_name in ebcdic.ignored_codec_names():
+        ebcdic_codec = ebcdic.lookup(codec_name)
+        assert ebcdic_codec is not None, codec_name
+        standard_codec = codecs.lookup(codec_name)
+        assert standard_codec is not None, codec_name
+        for code in range(256):
+            ebcdic_char = encoded(code, ebcdic_codec)
+            standard_char = encoded(code, standard_codec)
+            assert ebcdic_char == standard_char, f"{codec_name} at {code}: {ebcdic_char!r} != {standard_char!r}"
